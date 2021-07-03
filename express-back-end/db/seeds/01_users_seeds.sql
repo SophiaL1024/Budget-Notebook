a8eea9d6903228e9cd31cea45dc2230c3cd5b72b
sukick <suki.ckchan@gmail.com>
2021-07-03 18:32:53 +0000

    Add seeds from previous months to database

--- express-back-end/db/seeds/01_users_seeds.sql
+++ express-back-end/db/seeds/01_users_seeds.sql
@@ -1,5 +1,1 @@
 INSERT INTO users (email,password) VALUES ('alice@gmail.com','123');
-INSERT INTO users (email,password) VALUES ('carla@gmail.com','234');
-INSERT INTO users (email,password) VALUES ('anton@gmail.com','456');
-INSERT INTO users (email,password) VALUES ('angela@gmail.com','789');
-INSERT INTO users (email,password) VALUES ('ben@gmail.com','014');
