6bea1a55fbbd4c154c8d94faaf4c76fef8023f5f
SophiaL1024 <sophiafengliu@gmail.com>
2021-07-04 22:13:35 +0000

    delete node module under root folder

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import SideBar from './components/sideBar'
 
 class App extends Component {
   constructor(props) {
@@ -11,26 +12,27 @@ class App extends Component {
   }
 
   fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
+    axios.get('/dashboards/1') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
-      console.log(response.data) // The entire response from the Rails API
-
-      console.log(response.data.message) // Just the message
+      console.log(response.data) 
       this.setState({
-        message: response.data.message
+        message: response.data.annualExpenseSum
       });
     }) 
   }
 
   render() {
     return (
+      <>
+      <SideBar className="side-bar"/>
       <div className="App">
         <h1>{ this.state.message }</h1>
         <button onClick={this.fetchData} >
           Fetch Data
         </button>        
       </div>
+      </>
     );
   }
 }
