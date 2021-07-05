47b7c2ed74f7da4392d5ff798b0024e6874ce795
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-07-05 10:13:43 -0600

    attched routes to income and expense components

--- react-front-end/src/components/transactions/expenseList.jsx
+++ react-front-end/src/components/transactions/expenseList.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
-
+import {React, useEffect, useState }from 'react';
+import axios from 'axios';
 export default function expenseList() {
+
+  const [state, setState] = useState({
+    expenses:{}
+  }); 
+
+  useEffect(() => {
+    axios
+      .get("/transactions/1")
+      .then((res) => {
+        setState((prev) => ({ ...prev, expenses: res.data }));
+      });
+  }, []);
+ //function that loops over all expenses
 
   return(
   <table>
