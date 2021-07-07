ec10050f3b4588220a2102c9f8b7a09250817c3b
sukick <suki.ckchan@gmail.com>
2021-07-07 22:39:55 +0000

    Add style to sidebar and header

--- react-front-end/src/components/dashboard.jsx
+++ react-front-end/src/components/dashboard.jsx
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   paper: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(12),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
@@ -47,7 +47,7 @@ const Dashboard=function(){
         Expense :{state.dashboardData.currentMonthExpense }<br/>
       </Paper>
       <br/>
-      <Grid container spacing={3}>
+      <Grid container spacing={2}>
         <Grid item xs={12}>
         <Paper className={classes.paper}>
         Year: {year}<br/>
