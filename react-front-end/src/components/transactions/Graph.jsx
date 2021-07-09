c7ca5e9c30b4746a735ec7a1d9bb6c8c956d06f3
Dylan McGrann <dylanmcgrann@Dylans-MacBook-Pro.local>
2021-07-09 12:02:08 -0600

    added graph x axis info

--- react-front-end/src/components/transactions/Graph.jsx
+++ react-front-end/src/components/transactions/Graph.jsx
@@ -1,5 +1,57 @@
 import React, {useContext}  from "react";
-
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+const data = [
+  {
+    name: "January",
+    Expenses: 40,
+    Income: 2400,
+    amt: 2400
+  },
+  {
+    name: "March",
+    Expenses: 40,
+    Income: 2400,
+    amt: 2400
+  },
+  {
+    name: "April",
+    Expenses: 40,
+    Income: 2400,
+    amt: 2400
+  },
+  {
+    name: "May",
+    Expenses: 40,
+    Income: 2400,
+    amt: 2400
+  },
+  {
+    name: "June",
+    Expenses: 40,
+    Income: 2400,
+    amt: 2400
+  },
+  {
+    name: "July",
+    Expenses: 40,
+    Income: 2400,
+    amt: 2400
+  },
+  {
+    name: "August",
+    Expenses: 40,
+    Income: 2400,
+    amt: 2400
+  },
+];
 export default function Graph() {
 const {incomeTransactions, expenseTransactions} = useContext(dataContext);
 
